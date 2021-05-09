cdc179fd568c60a70bf908dc6673b464bfb35578
Georgiy Filippov <g.filippov777@gmail.com>
2021-05-09 12:11:13 +0500

    add tabs

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,40 @@
+const tabs = ({ tabContainer, tabToggle, tabItem, activeClass }) => {
+  const tabCont = document.querySelector(tabContainer);
+  const tabToggles = tabCont.querySelectorAll(tabToggle);
+  const tabItems = tabCont.querySelectorAll(tabItem);
+
+  function hideTabs() {
+    tabItems.forEach((item) => {
+      item.style.display = 'none';
+    });
+
+    tabToggles.forEach((item) => {
+      item.classList.remove(activeClass.replace(/\./, ''));
+    });
+  }
+
+  function showTabs(i = 0) {
+    tabItems[i].style.display = 'block';
+    tabToggles[i].classList.add(activeClass.replace(/\./, ''));
+  }
+
+  hideTabs();
+  showTabs();
+
+  tabCont.addEventListener('click', (evt) => {
+    const target = evt.target;
+
+    if (target && (target.matches(tabToggle) || target.closest(tabToggle))) {
+      evt.preventDefault();
+
+      tabToggles.forEach((item, index) => {
+        if (target === item || target.closest(tabToggle) === item) {
+          hideTabs();
+          showTabs(index);
+        }
+      });
+    }
+  });
+};
+
+export default tabs;
